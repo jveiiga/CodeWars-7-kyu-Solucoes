76a8e7694aaffcbcb21f4be40afc065a636ae5e8
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-05-31 18:06:14 -0300

    first commit

---
+++ Você é um quadrado!.js
@@ -0,0 +1,18 @@
+function isSquare(num) {
+  if( num < 0 ) {
+      return false
+  }
+  for( i = 0; i <= num; i++ ) {
+    result = i * i
+    if( result === num ){
+        return true;
+    } 
+  }
+  return false;
+}
+console.log(isSquare(-1));
+console.log(isSquare(0));
+console.log(isSquare(3));
+console.log(isSquare(4));
+console.log(isSquare(25));
+console.log(isSquare(26));
