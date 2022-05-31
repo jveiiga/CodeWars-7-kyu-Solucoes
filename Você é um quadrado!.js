eca919057a5dca8848f5420507b9613f8e7201c5
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-05-31 18:10:48 -0300

    Atualizando Você é um quadrado.js

--- Você é um quadrado!.js
+++ Você é um quadrado!.js
@@ -10,9 +10,9 @@ function isSquare(num) {
   }
   return false;
 }
-console.log(isSquare(-1));
-console.log(isSquare(0));
-console.log(isSquare(3));
-console.log(isSquare(4));
-console.log(isSquare(25));
-console.log(isSquare(26));
+console.log(isSquare(-1))
+console.log(isSquare(0))
+console.log(isSquare(3))
+console.log(isSquare(4))
+console.log(isSquare(25))
+console.log(isSquare(26))
